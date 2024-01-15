77c7a74f7c98ae31a2da109d41d7e25da268f8e3
Ramsés Rodríguez <x647175@gruposantander.com>
2024-01-15 15:55:37 +0100

    feat: remove middleware

--- src/middleware.ts
+++
@@ -1,7 +0,0 @@
-import { type MiddlewareHandler } from "astro";
-import { auth } from "./auth";
-
-export const onRequest: MiddlewareHandler = async (context, next) => {
-  context.locals.auth = auth.handleRequest(context);
-  return await next();
-};
