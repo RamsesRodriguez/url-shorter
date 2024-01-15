831a66a4322e2bbb270f8ccc093f46c45b9fe8bb
Ramsés Rodríguez <x647175@gruposantander.com>
2024-01-15 16:03:58 +0100

    fix: types

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,17 @@
 import type { Config } from "drizzle-kit";
 
-const dbCredentials = {
-  uri: `mysql://${process.env.DATABASE_USERNAME}:${process.env.DATABASE_PASSWORD}@${process.env.DATABASE_HOST}/url-shorter?ssl={"rejectUnauthorized":true}`,
+const dbConfig = {
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  host: process.env.DATABASE_HOST,
 };
 
 export default {
   schema: "./src/db/schema/index.ts",
   driver: "mysql2",
-  dbCredentials,
+  dbCredentials: {
+    uri: `mysql://${dbConfig.username}:${dbConfig.password}@${dbConfig.host}/url-shorter?ssl={"rejectUnauthorized":true}`,
+  },
   verbose: true,
   strict: true,
   out: "./src/db/migrations",
