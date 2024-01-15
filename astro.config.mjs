0d85e21dcee229f831998dbdc5f6561590ef2b09
Ramsés Rodríguez <x647175@gruposantander.com>
2024-01-15 16:44:09 +0100

    feat: added tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
-
 import auth from "auth-astro";
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [auth()],
+  integrations: [auth(), tailwind()]
 });
